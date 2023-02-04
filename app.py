e48737ea46549313dbb7baad6af72d3ab66d8453
lucas-dem <lucasd393939@gmail.com>
2023-02-04 11:13:40 -0300

    changes, only jpg to png working

--- app.py
+++ app.py
@@ -43,84 +43,110 @@ def jpg_to_png():
     return response
 
 
-
-
-
-@app.route('/api/pngtojpg', methods=['POST'])
-def png_to_jpg():
-    # Get the PNG image from the POST request
-    png_image = request.files.get('image')
-    # Open the PNG image with PIL
-    img = Image.open(png_image)
-    # Convert the PNG image to JPG
-    img = img.convert('RGB')
-    # Create a file-like buffer to receive JPG data.
-    buffer = io.BytesIO()
-    # Save the JPG image to the buffer
-    img.save(buffer, format='JPEG')
-    # Get the value of the buffer
-    jpg_image = buffer.getvalue()
-    # Close the buffer
-    buffer.close()
-    # Create a response with the JPG image as an attachment
-    response = send_file(io.BytesIO(jpg_image), attachment_filename='image.jpg', as_attachment=True)
-    response.headers.set('Content-Type', 'image/jpeg')
-    return response
-
 @app.route('/api/webptopng', methods=['POST'])
 def webp_to_png():
-    # Get the webp image from the POST request
-    webp_image = request.files['webp_image']
+    # Get the image file from the POST request
+    image = request.files.get('image')
 
-    # Open the webp image
-    with Image.open(webp_image) as img:
-        # Convert the image to PNG format
-        img = img.convert('RGB')
+    # Check if an image was uploaded
+    if not image:
+        return "No image selected"
+    
+    # Check if the image is in WEBP format
+    if not image.filename.endswith('.webp'):
+        return "Invalid image format. Please select a WEBP image."
+    
+    # Check if the image size is less than 5MB
+    if image.content_length > 5000000:
+        return "Image size too large. Please select an image less than 5MB."
 
-        # Create a BytesIO object to save the PNG image to
-        png_image = io.BytesIO()
-        img.save(png_image, 'PNG')
+    # Read the image file and convert it to a numpy array
+    image_bytes = image.read()
+    image = imageio.imread(image_bytes, format='webp', pilmode='L')
 
-        # Set the file cursor to the beginning of the BytesIO object
-        png_image.seek(0)
+    # Save the image as a PNG file
+    imageio.imwrite("converted_image.png", image, format='png')
 
-        # Return the PNG image as a downloadable attachment
-        return send_file(png_image, attachment_filename='converted.png', as_attachment=True, mimetype='image/png')
+    # Read the converted image and encode it in base64 format
+    with open("converted_image.png", "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+        
+    # Return the decoded image in the response as JSON
+    response = make_response(json.dumps({'image': encoded_string}))
+    response.headers['Content-Disposition'] = 'attachment; filename=converted_image.png'
+    return response
 
-@app.route("/api/bmptopng", methods=["POST"])
+
+@app.route('/api/bmptopng', methods=['POST'])
 def bmp_to_png():
-    # Get the BMP image from the request
-    bmp_image = request.files["image"]
+    # Get the image file from the POST request
+    image = request.files.get('image')
 
-    # Open the BMP image and convert it to PNG
-    with Image.open(bmp_image) as img:
-        img = img.convert("RGB")
-        png_image = io.BytesIO()
-        img.save(png_image, "PNG")
-        png_image.seek(0)
+    # Check if an image was uploaded
+    if not image:
+        return "No image selected"
+    
+    # Check if the image is in BMP format
+    if not image.filename.endswith('.bmp'):
+        return "Invalid image format. Please select a BMP image."
+    
+    # Check if the image size is less than 5MB
+    if image.content_length > 5000000:
+        return "Image size too large. Please select an image less than 5MB."
 
-    # Return the PNG image as a downloadable attachment
-    return send_file(png_image, mimetype="image/png", as_attachment=True, attachment_filename="converted.png")
+    # Read the image file and convert it to a numpy array
+    image_bytes = image.read()
+    image = imageio.imread(image_bytes, format='bmp')
+
+    # Save the image as a PNG file
+    imageio.imwrite("converted_image.png", image, format='png')
+
+    # Read the converted image and encode it in base64 format
+    with open("converted_image.png", "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+        
+    # Return the decoded image in the response as JSON
+    response = make_response(json.dumps({'image': encoded_string}))
+    response.headers['Content-Disposition'] = 'attachment; filename=converted_image.png'
+    return response
+
 
 @app.route('/api/pngtopdf', methods=['POST'])
-def convert_png_to_pdf():
-    # Get the PNG image from the request
-    image = request.files['image']
-    img = Image.open(image)
-    img = img.convert("RGB")
+def png_to_pdf():
+    # Get the image file from the POST request
+    image = request.files.get('image')
 
-    # Create a PDF document
-    pdf_buffer = BytesIO()
-    canvas_obj = canvas.Canvas(pdf_buffer)
+    # Check if an image was uploaded
+    if not image:
+        return "No image selected"
     
-    # Draw the image on the PDF
-    canvas_obj.drawImage(img, 0, 0)
-    canvas_obj.save()
-    pdf_buffer.seek(0)
+    # Check if the image is in PNG format
+    if not image.filename.endswith('.png'):
+        return "Invalid image format. Please select a PNG image."
+    
+    # Check if the image size is less than 5MB
+    if image.content_length > 5000000:
+        return "Image size too large. Please select an image less than 5MB."
 
-    response = Response(pdf_buffer.getvalue(), content_type='application/pdf')
-    response.headers.set("Content-Disposition", "attachment", filename="converted_file.pdf")
+    # Read the image file and convert it to a numpy array
+    image_bytes = image.read()
+    image = Image.open(BytesIO(image_bytes))
+
+    # Save the image as a PDF file
+    image_file = BytesIO()
+    canvas_obj = canvas.Canvas(image_file)
+    canvas_obj.drawInlineImage(image_bytes, 0, 0)
+    canvas_obj.save()
+
+    # Read the converted image and encode it in base64 format
+    encoded_string = base64.b64encode(image_file.getvalue()).decode("utf-8")
+        
+    # Return the decoded image in the response as JSON
+    response = make_response(json.dumps({'image': encoded_string}))
+    response.headers['Content-Type'] = 'application/json'
     return response
+
+
 
 @app.route("/", methods=["GET"])
 def index():
